0395fe9dff6f59112ded1925a306ccc3b1136dc8
Viknesh SR <vikneshsivaraman@gmail.com>
2025-04-19 21:37:43 +0530

    chnages

--- server/controller/userControler.js
+++ server/controller/userControler.js
@@ -10,7 +10,7 @@ export const create = async (req, res) => {
       return res.status(400).json({ message: "Place already posted" });
     }
 
-    const newPlace = new place({ name, maplink,State , description, District, path, filename });
+    const newPlace = new place({ name,maplink,State , description, District, path, filename });
     const savedData = await newPlace.save();
     res.status(200).json(savedData);
   } catch (error) {
@@ -32,7 +32,7 @@ export const getAllPlaces=async(req,res)=>{
     }
 };
 
-export const getPlace=async(req,res)=>{
+export const getbystate=async(req,res)=>{
     try{
           const {State}= req.params;
           const places = await place.find({State});
@@ -44,3 +44,20 @@ export const getPlace=async(req,res)=>{
       return  res.status(500).json({errorMessage:error.message});
 }
 };
+
+
+export const getbystateanddistrict=async(req,res)=>{
+  try{
+        const {State, District}= req.params;
+        const places = await place.find({State, District});
+        if(places.length===0){
+          return res.status(404).json({message: "Place not found"});
+      }
+      res.status(200).json(places);
+  } catch(error){
+    return  res.status(500).json({errorMessage:error.message});
+}
+};
+
+
+
